697d77a579112f2bde2cd094455702a33002dbc7
developer <developer@lidowin.com>
2023-07-27 10:21:10 +0000

    - add ips address whitelist auto update

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,150 @@
 mod config;
 mod proxy;
-use std::{net::SocketAddr, path::Path, fs::File, env, io::Read, sync::Arc};
 use config::Configuration;
+use std::{
+	collections::HashSet,
+	env,
+	fs::File,
+	io::Read,
+	net::{IpAddr, SocketAddr},
+	path::Path,
+	sync::{Arc, Mutex},
+	time::Duration,
+};
+use tokio::{
+	task::{self},
+	time::interval,
+};
 
-use hyper::{service::{service_fn, make_service_fn}, Server};
-
+use hyper::{
+	server::conn::AddrStream,
+	service::{make_service_fn, service_fn},
+	Server,
+};
 
 #[tokio::main]
 async fn main() {
-    // Get the CONFIG_SETTING environment variable
-    let config_setting = env::var("CONFIG_SETTING");
+	// Get the CONFIG_SETTING environment variable
+	let config_setting = env::var("CONFIG_SETTING");
 
-    let config: Configuration;
+	let config: Configuration;
 
-    match config_setting {
-        Ok(val) => {
-            // If CONFIG_SETTING is set, parse it
-            config = serde_json::from_str(&val).expect("JSON was not well-formatted");
-        },
-        Err(_) => {
-            // If CONFIG_SETTING is not set, parse the file
-            let json_file_path = Path::new("config.json");
-            let mut json_file = File::open(&json_file_path).expect("File open failed");
-            let mut json_content = String::new();
-            json_file.read_to_string(&mut json_content).expect("File read failed");
-            config = serde_json::from_str(&json_content).expect("JSON was not well-formatted");
-        }
-    }
+	match config_setting {
+		Ok(val) => {
+			// If CONFIG_SETTING is set, parse it
+			config = serde_json::from_str(&val).expect("JSON was not well-formatted");
+		}
+		Err(_) => {
+			// If CONFIG_SETTING is not set, parse the file
+			let json_file_path = Path::new("config.json");
+			let mut json_file = File::open(&json_file_path).expect("File open failed");
+			let mut json_content = String::new();
+			json_file
+				.read_to_string(&mut json_content)
+				.expect("File read failed");
+			config = serde_json::from_str(&json_content).expect("JSON was not well-formatted");
+		}
+	}
 
-    // clone port value from config
-    let config_port = config.http.servers[0].listen.clone();
+	let mut server_tasks = Vec::new();
 
-    let make_svc = make_service_fn(move |_| {
-        let config = Arc::new(config.clone());
-        async move {
-            Ok::<_, hyper::Error>(service_fn(move |req| proxy::mirror(req, Arc::clone(&config))))
-        }
-    });
+	let whitelisted_ips: Arc<Mutex<HashSet<IpAddr>>> = Arc::new(Mutex::new(HashSet::new()));
+	let client = reqwest::Client::new();
 
-    // get port from env to int or get from env
-    let port = config_port.parse::<u16>().unwrap_or_else(|_| env::var("PORT").unwrap_or_else(|_| "8080".to_string()).parse::<u16>().expect("PORT must be a number"));
+	for server in &config.http.servers {
+		let config_port = server.listen.clone();
+		let config = Arc::new(config.clone());
+		let whitelisted_ips = Arc::clone(&whitelisted_ips);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
-    let server = Server::bind(&addr).serve(make_svc);
+		let server_task = task::spawn(async move {
+			let make_svc = make_service_fn(move |socket: &AddrStream| {
+				let config = Arc::new(config.clone());
+				let whitelisted_ips = Arc::clone(&whitelisted_ips);
+				let remote_addr = socket.remote_addr().ip();
+				async move {
+					Ok::<_, hyper::Error>(service_fn(move |req| {
+						proxy::mirror(
+							req,
+							remote_addr.clone(),
+							Arc::clone(&whitelisted_ips),
+							Arc::clone(&config),
+						)
+					}))
+				}
+			});
 
-    if let Err(e) = server.await {
-        println!("error: {}", e);
-    }
+			// get port from env to int or get from env
+			let port = config_port.parse::<u16>().unwrap_or_else(|_| {
+				env::var("PORT")
+					.unwrap_or_else(|_| "8080".to_string())
+					.parse::<u16>()
+					.expect("PORT must be a number")
+			});
+
+			let addr = SocketAddr::from(([127, 0, 0, 1], port));
+			let server = Server::bind(&addr).serve(make_svc);
+
+			if let Err(e) = server.await {
+				println!("error: {}", e);
+			}
+		});
+		server_tasks.push(server_task);
+	}
+
+	let whitelist_updater_task = task::spawn(async move {
+		// Create an interval timer that ticks every 30 seconds
+		let mut interval = interval(Duration::from_secs(30));
+
+		loop {
+			// Wait for the next tick
+			interval.tick().await;
+
+			// Send the GET request
+			let res = client.get(config.ip_whitelist_url.clone()).send().await;
+
+			match res {
+				Ok(response) => {
+					// If the request was successful, parse the response body as a list of IPs
+					let body = match response.text().await {
+						Ok(body) => body,
+						Err(e) => {
+							eprintln!("Failed to parse response body: {}", e);
+							continue;
+						}
+					};
+
+					// Split the body by newline and parse each piece into an IP address
+					let ips: Vec<IpAddr> = body
+						.lines()
+						.map(|line| line.parse::<IpAddr>())
+						.filter_map(Result::ok)
+						.collect();
+
+					println!("updated whitelist: {:?}", ips.len());
+
+					// Lock the whitelist and update it
+					let mut whitelist = whitelisted_ips.lock().unwrap();
+					whitelist.clear();
+					for ip in ips {
+						whitelist.insert(ip);
+					}
+				}
+				Err(e) => {
+					eprintln!("Failed to send GET request: {}", e);
+				}
+			}
+		}
+	});
+
+	server_tasks.push(whitelist_updater_task);
+
+	let servers = futures::future::join_all(server_tasks).await;
+
+	for server in servers {
+		match server {
+			Ok(_) => println!("Server finished successfully"),
+			Err(e) => eprintln!("Server failed with error: {}", e),
+		}
+	}
+	// clone port value from config
 }
--- src/proxy.rs
+++ src/proxy.rs
@@ -1,191 +1,204 @@
-use std::{sync::Arc};
-use hyper::{Body, Request, Response, Client, HeaderMap, http::{HeaderValue, HeaderName}, StatusCode, client::HttpConnector};
+use hyper::{
+	client::HttpConnector,
+	http::{HeaderName, HeaderValue},
+	Body, Client, HeaderMap, Request, Response, StatusCode,
+};
 use hyper_tls::HttpsConnector;
-use tokio::fs::read;
 use mime_guess::from_path;
+use std::{
+	collections::HashSet,
+	net::IpAddr,
+	sync::{Arc, Mutex},
+};
+use tokio::fs::read;
 
 use crate::config::{Configuration, Proxy};
 
 // Asynchronous function named 'proxy'. It acts as a router for HTTP requests based on path
-pub async fn mirror(req: Request<Body>, config: Arc<Configuration>) -> Result<Response<Body>, hyper::Error> {
-    
-    // Create a new HTTP client to send requests
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, hyper::Body>(https);
+pub async fn mirror(
+	req: Request<Body>,
+	socket: IpAddr,
+	whitelisted_ips: Arc<Mutex<HashSet<IpAddr>>>,
+	config: Arc<Configuration>,
+) -> Result<Response<Body>, hyper::Error> {
+	// Create a new HTTP client to send requests
+	let https = HttpsConnector::new();
+	let client = Client::builder().build::<_, hyper::Body>(https);
 
-    // Extract the path component from the incoming HTTP request's URI
-    let path = req.uri().path();
+	println!("white list: {:?}", whitelisted_ips.lock().unwrap().len());
+	println!("socket: {:?}", socket);
 
-    // Iterate over all HTTP servers defined in the configuration
-    for server in &config.http.servers {
-        // Iterate over all proxies defined for the current server
-        for proxy in &server.proxies {
-            // Check if the request URI's path starts with the current proxy's path
-            if path.starts_with(&proxy.proxy_path) {
-                return proxy_request(req, client, proxy).await;
-            }
-        }
-        // serve the static files
-        return serve_static_files(path, &server.root).await;
-    }
-    handle(req).await   
+	// Extract the path component from the incoming HTTP request's URI
+	let path = req.uri().path();
+
+	// Iterate over all HTTP servers defined in the configuration
+	for server in &config.http.servers {
+		// Iterate over all proxies defined for the current server
+		for proxy in &server.proxies {
+			// Check if the request URI's path starts with the current proxy's path
+			if path.starts_with(&proxy.proxy_path) {
+				return proxy_request(req, client, proxy).await;
+			}
+		}
+		// serve the static files
+		return serve_static_files(path, &server.root).await;
+	}
+	handle(req).await
 }
-
 
 // Asynchronous function named 'handle'. It acts as a router for HTTP requests based on path
-async fn proxy_request(req: Request<Body>, client: Client<HttpsConnector<HttpConnector>>, proxy: &Proxy) -> Result<Response<Body>, hyper::Error> {
-    // Log the proxy
-    let full_url = &proxy.proxy_pass.clone();
-    let original_headers = req.headers().clone();
-    let query_params = req.uri().query().unwrap_or("");
+async fn proxy_request(
+	req: Request<Body>,
+	client: Client<HttpsConnector<HttpConnector>>,
+	proxy: &Proxy,
+) -> Result<Response<Body>, hyper::Error> {
+	// Log the proxy
+	let full_url = &proxy.proxy_pass.clone();
+	let original_headers = req.headers().clone();
+	let query_params = req.uri().query().unwrap_or("");
 
-    // add uri with query params
-    let path = req.uri().path();
-    let uri = match (proxy.retain_path, query_params.is_empty()) {
-        (true, true) => format!("{}", full_url),
-        (true, false) => format!("{}?{}", full_url, &query_params),
-        (false, true) => format!("{}{}", full_url, &path),
-        (false, false) => format!("{}{}?{}", full_url, &path, &query_params),
-    };
+	// add uri with query params
+	let path = req.uri().path();
+	let uri = match (proxy.retain_path, query_params.is_empty()) {
+		(true, true) => format!("{}", full_url),
+		(true, false) => format!("{}?{}", full_url, &query_params),
+		(false, true) => format!("{}{}", full_url, &path),
+		(false, false) => format!("{}{}?{}", full_url, &path, &query_params),
+	};
 
-    println!("requesting to uri: {}", uri);
+	println!("requesting to uri: {}", uri);
 
-    let request_result = Request::builder()
-        .method(req.method())
-        .uri(&uri)
-        .body(req.into_body());
+	let request_result = Request::builder()
+		.method(req.method())
+		.uri(&uri)
+		.body(req.into_body());
 
-    let mut request = match request_result {
-        Ok(req) => req,
-        Err(e) => {
-            // handle the error here, perhaps logging it and returning a response indicating the error
-            println!("Failed to construct the request: {}", e);
-            return Ok(Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body("Internal Server Error".into())
-                .unwrap());
-        },
-    };
+	let mut request = match request_result {
+		Ok(req) => req,
+		Err(e) => {
+			// handle the error here, perhaps logging it and returning a response indicating the error
+			println!("Failed to construct the request: {}", e);
+			return Ok(Response::builder()
+				.status(StatusCode::INTERNAL_SERVER_ERROR)
+				.body("Internal Server Error".into())
+				.unwrap());
+		}
+	};
 
-    // Get a mutable reference to the request's headers
-    let headers: &mut HeaderMap<HeaderValue> = request.headers_mut();
+	// Get a mutable reference to the request's headers
+	let headers: &mut HeaderMap<HeaderValue> = request.headers_mut();
 
-    // Copy all the headers from the original request
-    for (name, value) in original_headers {
-        // Convert the key to a HeaderName and the value to a HeaderValue
-        if let Some(header_name) = name {
-            // Skip the host header
-            if header_name == "host" {
-                continue;
-            }
-            headers.insert(header_name, value);
-        }
-    }
+	// Copy all the headers from the original request
+	for (name, value) in original_headers {
+		// Convert the key to a HeaderName and the value to a HeaderValue
+		if let Some(header_name) = name {
+			// Skip the host header
+			if header_name == "host" {
+				continue;
+			}
+			headers.insert(header_name, value);
+		}
+	}
 
-    // print all proxy
-    println!("proxy: {:?}", proxy);
+	// print all proxy
+	println!("proxy: {:?}", proxy);
 
-    // Check if the proxy has custom request headers defined
-    if let Some(request_headers) = &proxy.request_headers {
-        println!("Adding custom request headers");
-        println!("{:?}", request_headers);
-        // Iterate over each custom request header
-        for request_header in request_headers {
+	// Check if the proxy has custom request headers defined
+	if let Some(request_headers) = &proxy.request_headers {
+		println!("Adding custom request headers");
+		println!("{:?}", request_headers);
+		// Iterate over each custom request header
+		for request_header in request_headers {
+			// Check if the header has key-value pairs defined
+			if let Some(s_headers) = &request_header.headers {
+				// Iterate over each key-value pair in the header
+				for (key, value) in s_headers {
+					println!("{}: {}", key, value);
+					// Convert the key to a HeaderName and the value to a HeaderValue
+					let header_name = HeaderName::from_bytes(key.as_bytes()).unwrap();
+					let header_value: HeaderValue = HeaderValue::from_str(value).unwrap();
 
-            // Check if the header has key-value pairs defined
-            if let Some(s_headers) = &request_header.headers {
+					// Add the custom header to the request
+					headers.insert(header_name, header_value);
+				}
+			}
+		}
+	}
 
-                // Iterate over each key-value pair in the header
-                for (key, value) in s_headers {
-                    println!("{}: {}", key, value);
-                    // Convert the key to a HeaderName and the value to a HeaderValue
-                    let header_name = HeaderName::from_bytes(key.as_bytes()).unwrap();
-                    let header_value: HeaderValue = HeaderValue::from_str(value).unwrap();
-
-                    // Add the custom header to the request
-                    headers.insert(header_name, header_value);
-                }
-            }
-        }
-    }
-
-    // Send the request using the client and return the response
-    client.request(request).await
+	// Send the request using the client and return the response
+	client.request(request).await
 }
-
 
 // List of web file extensions
 const WEB_EXTENSIONS: [&str; 10] = [
-    ".html", 
-    ".js", 
-    ".css", 
-    ".png", 
-    ".jpg", 
-    ".jpeg", 
-    ".gif", 
-    ".svg", 
-    ".ico",
-    ".json"
+	".html", ".js", ".css", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico", ".json",
 ];
 
 // Asynchronous function named 'serve_static_files'. It acts as a router for HTTP requests based on path
-async fn serve_static_files(path: &str, folder_path: &String) -> Result<Response<Body>, hyper::Error> {
-    // Check if the path has a web file extension
-    let has_web_extension = WEB_EXTENSIONS.iter().any(|ext| path.ends_with(ext));
-    let file_path = if has_web_extension {
-        format!("{}{}", folder_path, path)
-    } else {
-        // format with added folder path
-        format!("{}/index.html", folder_path)
-    };
+async fn serve_static_files(
+	path: &str,
+	folder_path: &String,
+) -> Result<Response<Body>, hyper::Error> {
+	// Check if the path has a web file extension
+	let has_web_extension = WEB_EXTENSIONS.iter().any(|ext| path.ends_with(ext));
+	let file_path = if has_web_extension {
+		format!("{}{}", folder_path, path)
+	} else {
+		// format with added folder path
+		format!("{}/index.html", folder_path)
+	};
 
-    match read(&file_path).await {
-        Ok(bytes) => {
-            let mime_type = from_path(&file_path).first_or_octet_stream();
-            let mut response = Response::new(Body::from(bytes));
-            response.headers_mut().insert(
-                hyper::header::CONTENT_TYPE,
-                HeaderValue::from_str(mime_type.as_ref()).unwrap(),
-            );
-            Ok(response)
-        },
-        Err(e) => {
-            if has_web_extension {
-                // If it was a specific file and it failed to read, log the error
-                eprintln!("Failed to read file {}: {}", file_path, e);
-                // return response not found
-                return Ok(Response::builder()
-                    .status(StatusCode::NOT_FOUND)
-                    .body(Body::from("Not Found"))
-                    .unwrap());
-            }
+	match read(&file_path).await {
+		Ok(bytes) => {
+			let mime_type = from_path(&file_path).first_or_octet_stream();
+			let mut response = Response::new(Body::from(bytes));
+			response.headers_mut().insert(
+				hyper::header::CONTENT_TYPE,
+				HeaderValue::from_str(mime_type.as_ref()).unwrap(),
+			);
+			Ok(response)
+		}
+		Err(e) => {
+			if has_web_extension {
+				// If it was a specific file and it failed to read, log the error
+				eprintln!("Failed to read file {}: {}", file_path, e);
+				// return response not found
+				return Ok(Response::builder()
+					.status(StatusCode::NOT_FOUND)
+					.body(Body::from("Not Found"))
+					.unwrap());
+			}
 
-            // In case of an error or non-web extension path, fallback to index.html
-            // format the path with folder path
-            let back_path = format!("{}/index.html", folder_path);
-            let bytes = read(&back_path).await.unwrap_or_else(|e| {
-                eprintln!("Failed to read fallback file {}/index.html: {}", folder_path, e);
-                Vec::new()  // Empty Vec
-            });
-            let mime_type = from_path(&back_path).first_or_octet_stream();
-            let mut response = Response::new(Body::from(bytes));
-            response.headers_mut().insert(
-                hyper::header::CONTENT_TYPE,
-                HeaderValue::from_str(mime_type.as_ref()).unwrap(),
-            );
-            Ok(response)
-        }
-    }
+			// In case of an error or non-web extension path, fallback to index.html
+			// format the path with folder path
+			let back_path = format!("{}/index.html", folder_path);
+			let bytes = read(&back_path).await.unwrap_or_else(|e| {
+				eprintln!(
+					"Failed to read fallback file {}/index.html: {}",
+					folder_path, e
+				);
+				Vec::new() // Empty Vec
+			});
+			let mime_type = from_path(&back_path).first_or_octet_stream();
+			let mut response = Response::new(Body::from(bytes));
+			response.headers_mut().insert(
+				hyper::header::CONTENT_TYPE,
+				HeaderValue::from_str(mime_type.as_ref()).unwrap(),
+			);
+			Ok(response)
+		}
+	}
 }
 
 // handler req that returns "not mapped" response
 pub async fn handle(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let body = Body::from("not found");
+	let body = Body::from("not found");
 
-    // Create a Response with a 400 Bad Request status code
-    let response = Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(body).unwrap();
+	// Create a Response with a 400 Bad Request status code
+	let response = Response::builder()
+		.status(StatusCode::NOT_FOUND)
+		.body(body)
+		.unwrap();
 
-    Ok(response)
+	Ok(response)
 }
